9f4b19a48065398da70cf3367af9e99a1ebdaccc
512-kb <kb98kb98kb@gmail.com>
2020-02-17 20:26:17 +0530

    wh

--- routes.js
+++ routes.js
@@ -249,23 +249,22 @@ router.get("/user/getInfo", async (req, res) => {
 });
 
 router.post("/webhooks", async (req, res) => {
-  res.send(JSON.stringify(req));
-  // const signature = req.headers["stripe-signature"];
-  // try {
-  //   event = await stripe.webhooks.constructEvent(
-  //     JSON.stringify(req.body),
-  //     signature,
-  //     "whsec_gkgjKQSNDfinQs7QdgFvrKDtgGNWbtBz",
-  //     (err, event) => {
-  //       if (err) {
-  //         //console.log("colbek", err);
-  //         res.send(err);
-  //       } else res.send(event);
-  //     }
-  //   );
-  // } catch (err) {
-  //   res.send(err.message);
-  // }
+  const signature = req.headers["stripe-signature"];
+  try {
+    event = await stripe.webhooks.constructEvent(
+      JSON.stringify(req.body),
+      signature,
+      "whsec_gkgjKQSNDfinQs7QdgFvrKDtgGNWbtBz",
+      (err, event) => {
+        if (err) {
+          //console.log("colbek", err);
+          res.send(err);
+        } else res.send(event);
+      }
+    );
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 module.exports = router;
